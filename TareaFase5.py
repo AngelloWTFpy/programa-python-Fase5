0898f23cc30ffc51f6ce927b862c5bf69bd7449a
AngelloWTFpy <riverosmiguel120@gmail.com>
2026-05-25 10:50:24 -0500

    Agregar Variables 2

--- TareaFase5.py
+++ TareaFase5.py
@@ -2,5 +2,9 @@ print("Inicio de proyecto")
 
 nombre1 = "Miguel"
 nombre2 = "Ana"
+nombre3 = "Eduar"
+nombre4 = "Sofia"
+
 
 print("Hola", nombre1, "y", nombre2)
+print("Hola", nombre3, "y", nombre4)
